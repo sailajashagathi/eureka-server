7b6a48ae0ed303dbffb4f1a6c61de1c65bcda5f4
sailajashagathi <sailajas6263@gmail.com>
2025-05-28 12:28:14 -0500

    changes are made for this file

--- src/main/java/com/ust/eurekaserver/eureka_server/EurekaServerApplication.java
+++ src/main/java/com/ust/eurekaserver/eureka_server/EurekaServerApplication.java
@@ -10,6 +10,7 @@ public class EurekaServerApplication {
 
 	public static void main(String[] args) {
 		System.out.println("EurekaServerApplication.main");
+		System.out.println("updating the name");
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 
